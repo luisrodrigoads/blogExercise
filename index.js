7e1373cf702a5a73790ea308af147a04d1ecc781
luisrodrigoads <luisrodrigoads@gmail.com>
2020-08-10 23:26:14 -0300

    show and create data of db

--- index.js
+++ index.js
@@ -9,20 +9,61 @@ app.use(methodOverride("_method"));
 var bodyparser = require("body-parser");
 const port = process.env.PORT || "3000";
 
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/blogDB",{useNewUrlParser:true});
+
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine","ejs");
+
+var articleSchema = new mongoose.Schema({
+   title: String,
+   description: String
+});
+
+var Article = mongoose.model("Article",articleSchema);
 
 /**
  * Routes Definitions
  */
 
  app.get('/', function (req, res){
-    res.render('indexPage.ejs');
+    Article.find({},function(err, articles){
+       if(err){
+         console.log("Deu erro ao buscar os artigos");
+         console.log(err);
+       }else{
+         res.render('indexPage.ejs',{articles});
+       }
+    })
  });
 
- app.get('/otherPage', function(req,res){
-    res.render('otherPage.ejs');
+ app.get('/loginPage', function(req,res){
+    res.render('LoginPage.ejs');
+ });
+
+ app.get('/addArticlePage',function(req,res){
+    res.render('AddArticlePage.ejs');
+ });
+
+ app.post('/createArticle',function(req,res){
+   var title = req.body.title;
+   var description = req.body.description;
+   if(title != "" && description != ""){
+      Article.create({
+         title: title,
+         description: description
+      }, function(err, article){
+         if(err){
+            console.log("Deu erro no cadastro do artigo!");
+            console.log(err);
+         }else{
+            console.log("Artigo cadastrado com sucesso!");
+            console.log(article);
+         }
+         res.redirect('/');
+      });
+   }
  });
 
 /**
